6009bdcacd97a37f00f47d27d26acf4b7f054e78
rivo2302 <rivo2302@gmail.com>
2021-08-22 21:58:04 +0300

    Restructuration

--- src/bot-community/page.py
+++ src/bot-community/page.py
@@ -2,7 +2,7 @@ import urllib.parse
 from bs4 import BeautifulSoup
 from utils import *
 
-class WebSearch :
+class websearch :
     """
     Une classe qui va récupérer les liens de résultat 
     d'une recherche de google.
@@ -10,8 +10,33 @@ class WebSearch :
     def __init__(self) :
         self.source = 'www.google.com'
 
+
     @classmethod
-    def google(self,query):    
+    def search_image(self, query):
+        """ 
+        Une fonction qui récupère toutes les liens des images 
+        resultats selon les mot-clé en paramètre.
+        """
+        result = []
+        url = 'https://fr.images.search.yahoo.com/search/images;_ylt=AwrJS5dMFghcBh4AgWpjAQx.;_ylu=X3oDMTE0aDRlcHI2BGNvbG8DaXIyBHBvcwMxBHZ0aWQDQjY1NjlfMQRzZWMDcGl2cw--?p='+urllib.parse.quote(query)+'&fr2=piv-web&fr=yfp-t-905-s'
+        requete = get(url)
+        soup = BeautifulSoup(requete.text, 'html.parser')
+        container = soup.find("ul", {"id":"sres"})
+        try :
+            lis = container.find_all('li')
+        except :
+            return result
+        if len(lis)==0 : 
+            return result
+        for li in lis:
+            img = li.find("img")
+            img = str(img["data-src"]).split("&pid")
+            result.append(str(img[0]))  
+
+        return result 
+
+    @classmethod
+    def search_page(self,query):    
         """
         Une fonction qui récupère toutes les liens des  
         resultats selon les mot-clé en paramètre.
@@ -25,6 +50,8 @@ class WebSearch :
         for link in a :
             result.append(link["href"])
         return result
+
+
 if __name__ == "main":
     print("")
 
