2ceb5a3127342eb3f481ad0f9b1b8cf06820befa
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-08-23 23:50:54 +0300

    1ere Version Fonctionnel
    - Structuration en propriété de chaque fonction
    - un seul query pour un recherche
    - Fix Google search fausse resultat

--- websearch/script.py
+++ websearch/script.py
@@ -1,22 +1,20 @@
 import urllib.parse
 from bs4 import BeautifulSoup
+from .utils import get
 
 class WebSearch :
-    """
-    Une classe qui va récupérer les liens de résultat 
-    d'une recherche de google.
-    """
-    def __init__(self) :
-        self.source = 'www.google.com'
 
-    @classmethod
-    def search_image(self, query):
+    def __init__(self, query) :
+        self.query = query
+
+    @property
+    def images(self):
         """ 
         Une fonction qui récupère toutes les liens des images 
         resultats selon les mot-clé en paramètre.
         """
         result = []
-        url = 'https://fr.images.search.yahoo.com/search/images;_ylt=AwrJS5dMFghcBh4AgWpjAQx.;_ylu=X3oDMTE0aDRlcHI2BGNvbG8DaXIyBHBvcwMxBHZ0aWQDQjY1NjlfMQRzZWMDcGl2cw--?p='+urllib.parse.quote(query)+'&fr2=piv-web&fr=yfp-t-905-s'
+        url = 'https://fr.images.search.yahoo.com/search/images;_ylt=AwrJS5dMFghcBh4AgWpjAQx.;_ylu=X3oDMTE0aDRlcHI2BGNvbG8DaXIyBHBvcwMxBHZ0aWQDQjY1NjlfMQRzZWMDcGl2cw--?p='+urllib.parse.quote(self.query)+'&fr2=piv-web&fr=yfp-t-905-s'
         requete = get(url)
         soup = BeautifulSoup(requete.text, 'html.parser')
         container = soup.find("ul", {"id":"sres"})
@@ -33,18 +31,21 @@ class WebSearch :
 
         return result 
 
-    def search_page(self,query):    
+    @property
+    def pages(self):    
         """
         Une fonction qui récupère toutes les liens des  
         resultats selon les mot-clé en paramètre.
         """
         result = []
-        url = "https://www.google.com/search?client=firefox-b-d&q=" + urllib.parse.quote(query)
+        url = "https://www.google.com/search?client=firefox-b-d&q=" + urllib.parse.quote(self.query)
         requete = get(url)
         soup = BeautifulSoup(requete.text, 'html.parser')
         a = soup.find_all("a")
-
         for link in a :
-            result.append(link["href"])
+            tmp = link["href"][7:-1].split('&')
+
+            if tmp[0].startswith('http'):
+                result.append(tmp[0])
         return result
 
--- websearch/utils.py
+++ websearch/utils.py
@@ -5,7 +5,7 @@ def get (url, kwargs = {}):
     Une fonction qui get un liens avec les différents paramètres.
     """
     headers =  {
-        'User-agent' : 'Googlebot/2.1 (http://www.googlebot.com/bot.html)' 
+        'User-Agent' : 'Googlebot/2.1 (http://www.googlebot.com/bot.html)' 
         }  
     requete = gets(url, params = kwargs,headers = headers, timeout = 10) 
     return requete 
