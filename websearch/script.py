b9887c79e437310945ae6f7711f496d49f707c0e
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-08-24 00:35:35 +0300

    Optimisation
    Lors du deuxieme appel, si  le query n est pas changé,
     les fonctions renvoie les resultats sauvegardé par la derniere requete

--- websearch/script.py
+++ websearch/script.py
@@ -6,6 +6,8 @@ class WebSearch :
 
     def __init__(self, query) :
         self.query = query
+        # utiliser pour l'optimisation
+        self.__data = {}
 
     @property
     def images(self):
@@ -13,6 +15,9 @@ class WebSearch :
         Une fonction qui récupère toutes les liens des images 
         resultats selon les mot-clé en paramètre.
         """
+        if self.__data.get('images'):
+            if self.__data['images'][0] == self.query:
+                return self.__data['images'][1]
         result = []
         url = 'https://fr.images.search.yahoo.com/search/images;_ylt=AwrJS5dMFghcBh4AgWpjAQx.;_ylu=X3oDMTE0aDRlcHI2BGNvbG8DaXIyBHBvcwMxBHZ0aWQDQjY1NjlfMQRzZWMDcGl2cw--?p='+urllib.parse.quote(self.query)+'&fr2=piv-web&fr=yfp-t-905-s'
         requete = get(url)
@@ -28,7 +33,7 @@ class WebSearch :
             img = li.find("img")
             img = str(img["data-src"]).split("&pid")
             result.append(str(img[0]))  
-
+        self.__data['images'] = (self.query, result)
         return result 
 
     @property
@@ -37,6 +42,9 @@ class WebSearch :
         Une fonction qui récupère toutes les liens des  
         resultats selon les mot-clé en paramètre.
         """
+        if self.__data.get('pages'):
+            if self.__data['pages'][0] == self.query:
+                return self.__data['pages'][1]
         result = []
         url = "https://www.google.com/search?client=firefox-b-d&q=" + urllib.parse.quote(self.query)
         requete = get(url)
@@ -46,5 +54,6 @@ class WebSearch :
             tmp = link["href"][7:-1].split('&')
             if tmp[0].startswith('http'):
                 result.append(urllib.parse.unquote(tmp[0]))
+        self.__data['pages'] = (self.query, result)
         return result
 
